87faeca1b54b6a7a38722e40a5fbc204afa6e6aa
schro <schroederadam87@gmail.com>
2024-02-12 11:07:59 -0500

    Added the word alternate

--- src/eclipseSoloProject/HelloWorld.java
+++ src/eclipseSoloProject/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		
-		System.out.println("Hey look, it's some other stuff!");
+		System.out.println("Hey look, it's some other stuff! New and improved with the word alternate");
 	}
 
 }
